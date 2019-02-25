25d2cb95ceae64a5326e2fecc0b522bb938d105d
david <ds2643@columbia.edu>
2019-02-24 19:35:32 -0500

    Implement C0 interpreter

--- r1/interp.rkt
+++ r1/interp.rkt
@@ -4,63 +4,82 @@
 
 (require "utils.rkt")
 
-(define (interp-R1 env)
-  (define (interp-exp env)
-    (lambda (e)
-      (match e
-        [(? fixnum?) e]
-        [`(read)
-         (define r (read))
-         (if (fixnum? r)
-             r
-             (error `interp-R1 "input not an integer" r))]
-        [`(- ,e)
-         (define v ((interp-exp env) e))
-         (- 0 v)]
-        [`(- ,es ...)
-         (apply -
-                (for/list ([e es])
-                  ((interp-exp env) e)))]
-        [`(+ ,es ...)
-         (apply +
-                (for/list ([e es])
-                  ((interp-exp env) e)))]
-        [(? symbol?)
-         (let ([res (a-get e env)])
-           (if res
-               res
-               (error `interp-R1 "Symbol lookup failed:" e)))]
-        [`(let ([,x ,e]) ,body)
-         (define new-env
-           (cons (cons x ((interp-exp env) e))
-                 env))
-         ((interp-exp new-env) body)])))
-  (lambda (p)
-    (match p
-      [`(program ,info ,e)
-       ((interp-exp '()) e)])))
+(define (eval-exp env)
+  (lambda (e)
+    (match e
+      [(? fixnum?) e]
+      [`(read)
+       (define r (read))
+       (if (fixnum? r)
+           r
+           (error `interp-R1 "input not an integer" r))]
+      [`(- ,e)
+       (define v ((eval-exp env) e))
+       (- 0 v)]
+      [`(- ,es ...)
+       (apply -
+              (for/list ([e es])
+                ((eval-exp env) e)))]
+      [`(+ ,es ...)
+       (apply +
+              (for/list ([e es])
+                ((eval-exp env) e)))]
+      [(? symbol?)
+       (let ([res (a-get e env)])
+         (if res
+             res
+             (error `interp-R1 "Symbol lookup failed:" e)))]
+      [`(let ([,x ,e]) ,body)
+       (define new-env
+         (cons (cons x ((eval-exp env) e))
+               env))
+       ((eval-exp new-env) body)])))
 
-;; TODO: implement c0 interpreter
-(define (interp-C0 env)
-  (define (interp-exp env)
-    (lambda (e)
-      (match e
-        [else e])))
+(define (interp-R1 env)
   (lambda (p)
     (match p
       [`(program ,info ,e)
-       ((interp-exp '()) e)])))
+       ((eval-exp '()) e)])))
+
+(define (interp-C0 env)
+  (define (update-env env e)
+    (match e
+      [`(assign ,x ,b)
+       (cons `(,x . ,((eval-exp env) b))
+             env)]))
+  (define (interp-tail env)
+    (lambda (e)
+      (match e
+        [`(seq ,assignment ,tail)
+         (define new-env (update-env env assignment))
+         ((interp-tail new-env) tail)]
+        [`(return ,k)
+         ((eval-exp env) k)])))
+  (lambda (p)
+    (match p
+      [`(program ,info ,e)
+       ((interp-tail '()) e)])))
 
 (module+ test
-  (require rackunit
-           "test/test-programs.rkt")
+  (require rackunit)
 
-  (define answer-to-the-great-question
+  (define R1-example
     '(program ()
               (+ 45 (+ (+ (let ([y (+ 0 (- 15))])
                             y) 2)
                        10))))
 
-  (test-case "Example-based test"
-             (check-eq? ((interp-R1 '()) answer-to-the-great-question)
+  (define C0-example
+    '(program ()
+              (seq (assign x.1 (+ 2 2))
+                   (seq (assign x.2 38)
+                        (seq (assign y (+ x.1 x.2))
+                             (return y))))))
+
+  (test-case "Example-based test of interp-R1"
+             (check-eq? ((interp-R1 '()) R1-example)
+                        42))
+
+  (test-case "Example-based test of interp-C0"
+             (check-eq? ((interp-C0 '()) C0-example)
                         42)))
